5c0d162676d94745db39ef799a5370b16695e4a8
DaniilSvinsov <svdadm164@gmail.com>
2023-03-06 09:37:21 +0300

    commit №4

--- jsontocsv.py
+++ jsontocsv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-with open('Universitets.json', encoding='utf-8-sig') as inputfile:
+with open('grades.json', encoding='utf-8-sig') as inputfile:
     df = pd.read_json(inputfile)
 
-df.to_csv('Universitets1.csv', encoding='utf-8', index=False)
+df.to_csv('grades.csv', encoding='utf-8', index=False)
